570725585a17cdc76bda8ed060e67e29214b3ea4
ning666meng <103989196+ning666meng@users.noreply.github.com>
2025-12-16 22:54:49 +0800

    Update resolver_platform_test.go

--- pkg/deps/resolver_platform_test.go
+++ pkg/deps/resolver_platform_test.go
@@ -14,8 +14,8 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-- package deps_test
-+ package deps
+
+package deps
 
 
 import (
